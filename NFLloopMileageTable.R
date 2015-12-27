71a1167a4a7081b415110e1a8b9f7abe3704814e
dwtrue <dwtrue603@gmail.com>
2015-12-27 08:20:13 -0500

    Cleaned up startup script, functionalized the loop

--- NFLloopMileageTable.R
+++ NFLloopMileageTable.R
@@ -1,7 +1,9 @@
-for(x in 1:32) {
-        Team1<-as.character(NFLteamList[x,1])
-        for(y in 1:32) {
-                Team2<-as.character(NFLteamList[y,1])
-                                    PopMileageTable(Team1,Team2)
+NFLloopMileageTable<- function(NFLteamList) {
+        for(x in 1:32) {
+                Team1<-as.character(NFLteamList[x])
+                for(y in 1:32) {
+                        Team2<-as.character(NFLteamList[y])
+                        PopMileageTable(Team1,Team2)
+                }
         }
 }
